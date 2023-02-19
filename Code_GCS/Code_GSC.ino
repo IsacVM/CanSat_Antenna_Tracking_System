b7771537bd2973dfc5675bba47139fbddf1c70a8
alan23444 <alan.gonzalezcoro@gmail.com>
2023-02-19 05:37:15 -0600

    Adecuar el codigo a lo que necesitamos

--- Code_GCS/Code_GSC.ino
+++ Code_GCS/Code_GSC.ino
@@ -1,24 +1,24 @@
 
-#include <Servo.h>
+#include <Servo.h> // libreria para Servos
 Servo servo_Az, servo_El;  // Creamos un objeto de clase "Servo" llamado "myservo" para controlar nuestro servomotor
-int potpin = 0;  // Pin analógico al cual conectamos la señal de nuestro potenciómetro
-int val;    // variable que contendrá el valor leido en el pin del potenciómetro y posteriormente el valor que escribiremos al servomotor
-
-
-float elevacion=60;
-float azimut=180;
+int servoPin_Az =  ; // variable que almacena el numero del pin PWM del arduino donde esta conectado el servo_Az
+int servoPin_El =  ; // variable que almacena el numero del pin PWM del arduino donde esta conectado el servo_El
+float servoPos_Az ; // variable que almacena el valor del angulo de azimuth
+float servoPos_El ; // variable que almacena el valor del angulo de elevation
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
-  servo_Az.attach(9);  // Adjuntamos el objeto que creamos al pin 9
-  servo_El.attach(9);  // Adjuntamos el objeto que creamos al pin 9
-
+  servo_Az.attach(servoPin_Az);  // Adjuntamos el objeto que creamos al pin -- 
+  servo_El.attach(servoPin_El);  // Adjuntamos el objeto que creamos al pin --
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // ----- formulas para calcular el angulo de azimuth y elevation
+  servo_Az.write(servoPos_Az);
+  servo_El.write(servoPos_El);
+
   print( "Obtención de Datos ...");
 
   //Sección Rx_Data from Cansat
